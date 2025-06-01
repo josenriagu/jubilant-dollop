70620d962ab87571bb55f8bd73eb9cc06501cf26
Josemaria Nriagu <josemaria.nriagu@akasha.world>
2025-06-01 21:25:58 +0100

    chore: update config - update constants

--- app/index.tsx
+++ app/index.tsx
@@ -6,38 +6,11 @@ import {
   AppKit,
 } from "@reown/appkit-ethers-react-native";
 import Authentication from "~/components/authentication";
+import { chains, metadata } from "~/lib/constants";
 
 const projectId = process.env.PUBLIC_WALLET_CONNECT_PROJECT_ID ?? "";
 
-const metadata = {
-  name: "Authentication | Akasha",
-  description: "Sample Expo Project",
-  url: "https://expo.dev",
-  icons: ["https://avatars.githubusercontent.com/u/179229932"],
-  redirect: {
-    native: "exp://",
-  },
-};
-
 const config = defaultConfig({ metadata });
-
-const mainnet = {
-  chainId: 1,
-  name: "Ethereum",
-  currency: "ETH",
-  explorerUrl: "https://etherscan.io",
-  rpcUrl: "https://cloudflare-eth.com",
-};
-
-const polygon = {
-  chainId: 137,
-  name: "Polygon",
-  currency: "POL",
-  explorerUrl: "https://polygonscan.com",
-  rpcUrl: "https://polygon-rpc.com",
-};
-
-const chains = [mainnet, polygon];
 
 createAppKit({
   projectId,
--- lib/constants.ts
+++ lib/constants.ts
@@ -1,18 +1,47 @@
 export const NAV_THEME = {
   light: {
-    background: 'hsl(0 0% 100%)', // background
-    border: 'hsl(240 5.9% 90%)', // border
-    card: 'hsl(0 0% 100%)', // card
-    notification: 'hsl(0 84.2% 60.2%)', // destructive
-    primary: 'hsl(240 5.9% 10%)', // primary
-    text: 'hsl(240 10% 3.9%)', // foreground
+    background: "hsl(0 0% 100%)", // background
+    border: "hsl(240 5.9% 90%)", // border
+    card: "hsl(0 0% 100%)", // card
+    notification: "hsl(0 84.2% 60.2%)", // destructive
+    primary: "hsl(240 5.9% 10%)", // primary
+    text: "hsl(240 10% 3.9%)", // foreground
   },
   dark: {
-    background: 'hsl(240 10% 3.9%)', // background
-    border: 'hsl(240 3.7% 15.9%)', // border
-    card: 'hsl(240 10% 3.9%)', // card
-    notification: 'hsl(0 72% 51%)', // destructive
-    primary: 'hsl(0 0% 98%)', // primary
-    text: 'hsl(0 0% 98%)', // foreground
+    background: "hsl(240 10% 3.9%)", // background
+    border: "hsl(240 3.7% 15.9%)", // border
+    card: "hsl(240 10% 3.9%)", // card
+    notification: "hsl(0 72% 51%)", // destructive
+    primary: "hsl(0 0% 98%)", // primary
+    text: "hsl(0 0% 98%)", // foreground
+  },
+};
+
+const mainnet = {
+  chainId: 1,
+  name: "Ethereum",
+  currency: "ETH",
+  explorerUrl: "https://etherscan.io",
+  rpcUrl: "https://cloudflare-eth.com",
+};
+
+const polygon = {
+  chainId: 137,
+  name: "Polygon",
+  currency: "POL",
+  explorerUrl: "https://polygonscan.com",
+  rpcUrl: "https://polygon-rpc.com",
+};
+
+export const chains = [mainnet, polygon];
+
+export const metadata = {
+  name: "Authentication | Akasha",
+  description: "Sample Expo Project",
+  url: "https://akasha.org",
+  icons: ["https://avatars.githubusercontent.com/u/179229932"],
+  redirect: {
+    native: "expornr://",
+    universal: "akasha.org",
   },
 };
